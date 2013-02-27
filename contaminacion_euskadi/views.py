8ff47e269c2e6d1b84e2762c3efcbe74b0d3d909
Oscar <oscar.pena@deusto.es>
2013-02-27 16:27:19 +0100

    Deleted prints from views.py

--- contaminacion_euskadi/views.py
+++ contaminacion_euskadi/views.py
@@ -15,9 +15,21 @@ from app.models import AirPollution
 from app.forms import ParticleForm
 
 
+limits = {
+    'Benzene': 0,
+    'CO': 10000,
+    'Ethylbenzene': 0,
+    'NH3': 0,
+    'NMHC': 0,
+    'PM10': 50,
+    'SH2': 0,
+    'Toluene': 0,
+    'Xylene': 0,
+}
+
+
 def home(request):
     points = []
-    limit = '0'
     if request.method == 'POST':
         form = ParticleForm(request.POST)
         if form.is_valid():
@@ -27,34 +39,16 @@ def home(request):
             start_date = cd['start_date']
             end_date = cd['end_date']
 
-            if particle == 'SO2':
-                limit = '125'
-            if particle == 'NO2':
-                limit = '200'
-            if particle == 'PM10':
-                limit = '50'
-            if particle == 'CO':
-                limit = '10'
+            json_response = json.load(urllib2.urlopen('http://helheim.deusto.es/bizkaisense/api/outlimit_stations/' + particle + '/' + start_date + '/' + end_date + '/' + str(limits[particle]) + ''))
 
-            json_response = json.load(urllib2.urlopen('http://helheim.deusto.es/bizkaisense/api/outlimit_stations/' + particle + '/' + start_date + '/' + end_date + '/' + limit + ''))
-
-            municipios = AirPollution.objects.filter(descripcion=particle).values('municipio')
+            municipios = AirPollution.objects.filter(descripcion = particle).values('municipio')
 
             for element in json_response:
-                print element['municipality']
-                #if str(element['municipality']) in municipios.values():
-                    #points.append(element)
                 found = False
                 for mun in municipios.values():
-                    print "--->", mun['municipio']
                     found = found or (mun['municipio'] == element['municipality'])
                 if found:
                     points.append(element)
-
-            print '------------------------------------'
-            print municipios
-            print '------------------------------------'
-            print points
 
             return render_to_response("app/index.html", {
                     'form': form,
